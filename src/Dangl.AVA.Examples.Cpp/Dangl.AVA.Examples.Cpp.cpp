66a682e5d01ca4c6ce554d7a6da3aad780373967
Georg Dangl <georg@dangl.me>
2019-05-24 21:28:47 +0200

    Use CLI args for GAEB paths instead of hardcoded strings

--- src/Dangl.AVA.Examples.Cpp/Dangl.AVA.Examples.Cpp.cpp
+++ src/Dangl.AVA.Examples.Cpp/Dangl.AVA.Examples.Cpp.cpp
@@ -12,10 +12,16 @@ using namespace Dangl::AVA::Contents::ServiceSpecificationContents;
 
 int main(array<System::String ^> ^args)
 {
-	char inputGaebFile[] = "C:\\Users\\Georg\\Git\\Dangl.AVA.Examples.Cpp\\GaebFiles\\GAEBXML_DE.X86";
-	char outputGaebFile[] = "C:\\Users\\Georg\\Git\\Dangl.AVA.Examples.Cpp\\GaebExport.X86";
+	if (args->Length != 2) {
+		Console::WriteLine(L"Please supply two command line arguments:");
+		Console::WriteLine(L"1. Path to a GAEB file to import");
+		Console::WriteLine(L"2. Path to the GAEB file to export");
+		Console::WriteLine(L"In Visual Studio, you can use these settings:");
+		Console::WriteLine(L"\"$(SolutionDir)\\GaebFiles\\GAEBXML_DE.X86\" \"$(SolutionDir)\\GaebExport.X86\"");
+		return 1;
+	}
 
-	Dangl::AVA::Project^ project = Dangl::AVA::Interop::AvaConverter::ConvertGaebToAva(gcnew System::String(inputGaebFile));
+	Dangl::AVA::Project^ project = Dangl::AVA::Interop::AvaConverter::ConvertGaebToAva(args[0]);
 
 	array<Position^> ^positions = AvaConverter::GetPositionsInProject(project);
 
@@ -42,8 +48,8 @@ int main(array<System::String ^> ^args)
 	Dangl::AVA::Contents::ServiceSpecificationContents::PositionExtensions::SetQuantity(position, *gcnew System::Decimal(5));
 	servSpec->Elements->Add(position);
 
-	Dangl::AVA::Interop::AvaConverter::ExportToGaeb(projectToExport, gcnew System::String(outputGaebFile), GaebTargetType::GaebXml);
-	Console::WriteLine(gcnew System::String(outputGaebFile));
+	Dangl::AVA::Interop::AvaConverter::ExportToGaeb(projectToExport, args[1], GaebTargetType::GaebXml);
+	Console::WriteLine(args[1]);
 
 	return 0;
 }
